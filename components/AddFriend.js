c3701c5d589cbb01147a4ad86ecda961aaf9e6e8
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-15 12:24:02 -0700

    AddFriend.js: implement form submit logic

--- components/AddFriend.js
+++ components/AddFriend.js
@@ -1,13 +1,15 @@
 
 
-// Add the second handler function parameter and get handleAddFriend
-export default function createAddFriend(root) {
+export default function createAddFriend(root, { handleAddFriend }) {
     const form = root.querySelector('form');
 
     form.addEventListener('submit', (e) => {
-        // *** prevent the form's default behavior of changing the browser page
-        // create a formData object
-        // call the handler with the "name" value from the formData
+        e.preventDefault();
+
+        const formData = new FormData(form);
+        handleAddFriend(formData.get('name'));
+
+        form.reset();
     });
 
     return () => { };
